f25c7a66a1325242e1b5c7f979d54c04b76945fd
zanwonzzzz <etnicastro9@gmail.com>
2025-07-15 12:53:51 -0600

    ..

--- src/views/Cliente.vue
+++ src/views/Cliente.vue
@@ -1,25 +1,9 @@
 <template>
 <navbar></navbar>
-<button name="Cliente_Lo" id="Cliente_Lo" @click="showMessage" v-if="bandera">Cliente Localizado</button>
-<button name="Cliente_NoLo" v-if="bandera">Cliente No Localizado</button>
+<button name="Cliente_Lo" id="Cliente_Lo" @click="OnSubmit($event)" v-if="bandera">Cliente Localizado</button>
+<button name="Cliente_NoLo" id="Cliente_NoLo" v-if="bandera" @click="OnSubmit($event)">Cliente No Localizado</button>
 
-<form @submit.prevent="Localizado">
-    <label for="">¿Le solicitaron algun cobre por la instalacion?</label>
-    <select name="P_Cobro_Instalacion" id="P_Cobro_Instalacion">
-        <option value="">Selecciona una respuesta</option>
-        <option value="SI">SI</option>
-        <option value="NO">NO</option>
-    </select>
-    <label for="">¿Pudo accesar al domicilio?</label>
-    <select name="Acceso_Domicilio" id="Acceso_Domicilio">
-        <option value="">Selecciona una respuesta</option>
-        <option value="SI">SI</option>
-        <option value="NO">NO</option>
-    </select>
-    <button type="submit">Avanzar</button>
-</form>
-
-<form @submit.prevent="NoLocalizado">
+<form id="FormLocalizado" v-if="!bandera" @submit.prevent="Localizado">
     <label for="">¿Le solicitaron algun cobre por la instalacion?</label>
     <select name="P_Cobro_Instalacion" id="P_Cobro_Instalacion">
         <option value="">Selecciona una respuesta</option>
@@ -41,7 +25,25 @@ import navbar from '../components/navbar.vue';
 import { useRoute,useRouter } from 'vue-router';
 import { ref } from 'vue';
 
+const router = useRouter()
 const bandera = ref(true)
+function OnSubmit($event)
+{
+    var boton = showMessage($event)
+    LocalizadoN(boton)
 
-const showMessage = (event) => alert(event.target.id);
+}
+//const showMessage = (event) => alert(event.target.name);
+function showMessage(event)
+{
+   const nombre = event.target.name
+   return nombre
+}
+
+function LocalizadoN(nombre)
+{
+    if(nombre == "Cliente_Lo"){bandera.value = false}
+    else {router.push('/ordenes')}
+}
+
 </script>
---
+++ src/views/InteriorFibra.vue
@@ -0,0 +1,19 @@
+<template>
+<!--este en realidad queria que fuera componente pero como ya necesito acabar esto asi se ba a ir
+y acabando todo valido y luego ya corrigo si hay malas practicas-->
+<form action="">
+    <label for="">¿LOS LEDS DEL MODEM ESTAN EN VERDE?</label>
+    <select name="" id="">
+        <option value="">SELECCIONA LA RESPUESTA</option>
+        <option value="">SI</option>
+        <option value="">NO</option>
+    </select>
+    <label for="">¿SE INSTALO ROSETA?</label>
+    <select name="" id="">
+        <option value="">SELECCIONA LA RESPUESTA</option>
+        <option value="">SI</option>
+        <option value="">NO</option>
+    </select>
+    <button></button>
+</form> 
+</template>
